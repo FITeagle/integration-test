cc2b2d947ba9b968a4865488231568912584e366
Bjoern Riemer <bjoern.riemer@tu-berlin.de>
2015-09-24 10:58:42 +0200

    code cleanup

--- runJfed_local.sh
+++ runJfed_local.sh
@@ -19,33 +19,38 @@ java \
   --debug \
   --context-file conf/cli.properties \
   --group nonodelogin
+#createsliver
 
 RET=$?
 echo "jfed error code ${RET}"
 
 DIR=$(ls -td test-result*|head -n1)
 if [[ $(grep " failheader" -c ./${DIR}/result.html) > 0 ]]; then
+  
   echo "test failed!"
+
   if [[ -f /.dockerenv ]] && [[ -d /opt/results ]]; then
     cp -a $DIR /opt/results
     echo "PWD: $PWD"
-    ##/opt/fiteagle/integration-test/build/server/wildfly/standalone/deployments/motor.war
     ##PWD: /opt/fiteagle/integration-test
     cp ./build/server/wildfly/standalone/log/server.log /opt/results
     chmod o+w -R /opt/results
   fi
+
   if [[ -n ${TRAVIS_BUILD_DIR} ]]; then
     printf "\n#############################\n## uploading results ########\n#############################\n\n"
     echo "results.html: "
     curl http://foo:bar@demo.fiteagle.org:8081/paste -X POST -T ${DIR}/result.html
     echo ""
     echo "server.log: "
-    curl http://foo:bar@demo.fiteagle.org:8081/paste -X POST -T "${TRAVIS_BUILD_DIR}/*/server/wildfly/standalone/log/server.log"
     curl http://foo:bar@demo.fiteagle.org:8081/paste -X POST -T "${WILDFLY_HOME}/standalone/log/server.log"
     printf "\n#############################\n## end ######################\n#############################\n\n"
   fi
+
 else
   echo "test OK"
 fi
+
 if [[ "$OSTYPE" == "darwin"* ]]; then open "./${DIR}/result.html" ; fi
+
 exit $RET
